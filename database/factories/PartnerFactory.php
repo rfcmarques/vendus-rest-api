286d150d19f305ab75a89b5a5742215f62c799ce
Rui Marques <a.marques1967@gmail.com>
2024-04-14 18:11:36 +0100

    Created the Partner model and the corresponding migration, factory and seeder

---
+++ database/factories/PartnerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Partner>
+ */
+class PartnerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'vat' => $this->faker->numberBetween(100000000, 999999999),
+            'email' => $this->faker->companyEmail(),
+            'address' => $this->faker->address(),
+            'comission' => $this->faker->randomFloat(2, 0, 100),
+        ];
+    }
+}
