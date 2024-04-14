093f655f058031aeff06b110bcf176cb57731b51
Rui Marques <a.marques1967@gmail.com>
2024-04-14 18:31:09 +0100

    Added the partners resource endpoint

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\PartnerController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::resource('partners', PartnerController::class);
