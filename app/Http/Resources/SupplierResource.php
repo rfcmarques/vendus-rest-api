4ba5cc5ac93b38b7a949fa037f24eaae0facb267
Rui Marques <a.marques1967@gmail.com>
2024-04-14 20:59:28 +0100

    Created the supplier resource

---
+++ app/Http/Resources/SupplierResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SupplierResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'max_due_days' => $this->max_due_days,
+            'contract_file' => $this->contract_file
+        ];
+    }
+}
