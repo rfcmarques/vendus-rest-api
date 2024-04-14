4a72fbea867895cd4784198ade9b9d12ae99e0e6
Rui Marques <a.marques1967@gmail.com>
2024-04-14 19:54:02 +0100

    Changed the fillable fields for Partner controller

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Partner extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'vat',
+        'email',
+        'address',
+        'comission'
+    ];
 }
