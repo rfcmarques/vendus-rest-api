d40bba3d57541a49af2c08f356406c6c85bd5d65
Rui Marques <a.marques1967@gmail.com>
2024-04-14 20:41:12 +0100

    Created the Customer controller and the CRUD functions

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\CustomerResource;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CustomerResource::collection(Customer::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        $customer = Customer::create($request->validated());
+
+        return CustomerResource::make($customer);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Customer $customer)
+    {
+        return CustomerResource::make($customer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->validated());
+
+        return CustomerResource::make($customer);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+
+        return response()->noContent();
+    }
+}
