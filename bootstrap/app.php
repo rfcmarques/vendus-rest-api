3fa1f933354eabf3f891a5926f3d1818ef7ae86d
Rui Marques <a.marques1967@gmail.com>
2024-04-14 23:12:15 +0100

    Added the exception handling for the query builder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\QueryBuilder\Exceptions\InvalidFilterQuery;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -19,6 +20,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->renderable(function (NotFoundHttpException $e, $request) {
             if ($request->is('api/*')) {
                 return response()->json(['message' => 'Object not found'], 404);
+            }
+        });
+
+        $exceptions->renderable(function (InvalidFilterQuery $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => $e->getMessage()], 400);
             }
         });
     })->create();
