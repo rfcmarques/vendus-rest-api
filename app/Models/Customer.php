d865530f6755433eaaec2f3998f68217ad5ff5fb
Rui Marques <a.marques1967@gmail.com>
2024-04-14 23:27:32 +0100

    Added the mass assignment fields

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'vat',
+        'email',
+        'address',
+        'partner_id',
+        'discount'
+    ];
 }
